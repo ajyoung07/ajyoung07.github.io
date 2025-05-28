a61ee4d1e1a3ca5da44bdda9a8f8913327cc1a60
AJ Young <albertjyoung1@gmail.com>
2025-05-27 20:56:15 -0400

    Added Dark Mode toggle

--- main.js
+++ main.js
@@ -14,4 +14,12 @@ document.addEventListener("DOMContentLoaded", () =>{
     document.getElementById("linkedin").addEventListener('click', () => window.open("https://www.linkedin.com/in/albert-young-6b5681255/", target="_blank"))
     document.getElementById("github").addEventListener('click', () => window.open("https://github.com/ajyoung07", target="_blank"))
     document.getElementById("email").addEventListener('click', () => window.open('mailto:alberjyoung1@gmail.com', target='_blank'));
+
+    document.getElementById("toggle-dark-mode").addEventListener('click', () => {
+        const icon_elem = document.getElementById("icon-name");
+        let txt = icon_elem.innerText;
+        if (txt === "dark_mode") txt = "light_mode";
+        else txt = "dark_mode";
+        icon_elem.innerText = txt;
+    })
 })
